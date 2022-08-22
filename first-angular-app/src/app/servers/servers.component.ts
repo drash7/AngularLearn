c447deb60d1ed74fc55331c22d2da20cb4813f60
Daniel Sharvaaya Dash <daniel.s.dash@gmail.com>
2022-08-22 15:50:15 -0400

    Explored Databinding

--- first-angular-app/src/app/servers/servers.component.ts
+++ first-angular-app/src/app/servers/servers.component.ts
@@ -2,18 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() =>  {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer()  {
+    this.serverCreationStatus = 'Server was created! Name: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event)  {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
