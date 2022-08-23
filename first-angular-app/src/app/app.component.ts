cdaabc01623da066512353dc926b5502b1edd177
Daniel <daniel.s.dash@gmail.com>
2022-08-22 23:58:01 -0400

    Finished introductory directives exploration

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -5,12 +5,34 @@ import { Component } from '@angular/core';
   // can select by class by doing .app-root
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
   // styles: [`
   //   h3  {
   //     color: dodgerblue;
   //   }
   // `]
+  styles: [`
+    .future {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+display = false;
+btnClickCount = 0
+btnClicks = []
+
+  onDisplayDetails(Event: any)  {
+    this.display = !this.display;
+    this.btnClickCount += 1;
+    this.btnClicks.push("Button click: " + this.btnClickCount);
+  }
+
+  getBackgroundColor(i)  {
+    return i > 4 ? 'blue' : 'white';
+  }
+
+  getColor(i)  {
+    return i > 4 ? 'white' : 'black';
+  }
 }
