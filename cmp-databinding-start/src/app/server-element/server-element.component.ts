01a8f9881afb3187f7c61b759f04f20b608292fa
Daniel Sharvaaya Dash <daniel.s.dash@gmail.com>
2022-09-07 16:03:59 -0400

    Halfway through 79

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   // the @Input decorator can be used to bind to our own properties
   // can use an alias provided within parentheses to refer to the property as something else outside this component
   @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+  @Input() name: string;
+
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!')
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
   }
 
 }
